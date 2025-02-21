1b09b729735051a6e8dbf51e6924214d7891df97
Morgan Purcell <morganfpurcell@gmail.com>
2025-02-20 18:51:59 -0800

    Update movie hut project + styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import '../styles/global.css';
 import { FaLink } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa';
 import fuelAIImg from '../assets/projects/fuel-ai.png';
 import movieHubImg from '../assets/projects/movie-hub.png';
 import expendoImg from '../assets/projects/expendo.png';
@@ -16,7 +17,7 @@ function Projects() {
     {
       image: fuelAIImg,
       name: 'Fuel AI',
-      link: 'https://gofuel.ai',
+      projectLink: 'https://gofuel.ai',
       about:
         'Fuel AI is a data marketplace where companies request custom datasets, and users earn by collecting data. As a Founding Full Stack Engineer, I helped build and scale the platform, growing the user base from 0 to 10,000+ across 100+ countries.',
       tech: [
@@ -29,8 +30,9 @@ function Projects() {
     },
     {
       image: movieHubImg,
-      name: 'Movie Hub',
-      link: 'https://github.com/Morganfp/movie-hub',
+      name: 'Movie Hut',
+      githubLink: 'https://github.com/Morganfp/movie-hut',
+      projectLink: 'https://moviehut.morganfpurcell.com/',
       about:
         'A movie discovery application that allows users to search for movies by title and view key details such as release year, rating, and cover images. Built with React, this app offers a clean, responsive UI and seamless integration with the Movie Database (TMDB) API.',
       tech: [
@@ -41,7 +43,7 @@ function Projects() {
     {
       image: expendoImg,
       name: 'Expendo',
-      link: 'https://github.com/Morganfp/expendo-finance-tracker',
+      githubLink: 'https://github.com/Morganfp/expendo-finance-tracker',
       about:
         'A finance tracking application that helps users manage their finances by recording income and expenses. Built with React, this app features light/dark mode, persistent data storage, and intuitive transaction management.',
       tech: [
@@ -52,7 +54,7 @@ function Projects() {
     {
       image: coachesCornerImg,
       name: 'Coaches Corner',
-      link: null,
+      githubLink: null,
       about:
         'I worked as a backend engineer with a team of 5 to build a scalable web application using Django, Python, and MySQL in an Agile environment, efficiently tracking and visualizing over 1,100 athlete performance metrics at Lindenwood University.',
       tech: [
@@ -64,7 +66,7 @@ function Projects() {
     {
       image: moWeatherImg,
       name: 'MoWeather',
-      link: 'https://github.com/Morganfp/moweather-weather-app',
+      githubLink: 'https://github.com/Morganfp/moweather-weather-app',
       about:
         'A weather app that allows users to search for locations and view the current weather, including temperature, city, and country. Built with React, this app fetches weather data from the Open-Meteo API and location data from OpenStreetMap.',
       tech: [
@@ -75,7 +77,7 @@ function Projects() {
     {
       image: ekoChatbotImg,
       name: 'Eko',
-      link: 'https://github.com/Morganfp/eko-chatbot',
+      githubLink: 'https://github.com/Morganfp/eko-chatbot',
       about:
         'Eko Chatbot is a customizable customer support chatbot widget built with React, TypeScript, Python, and FastAPI. Designed to help businesses automate responses to common inquiries, Eko enables efficient customer interactions without the need for AI models.',
       tech: [
@@ -88,7 +90,7 @@ function Projects() {
     {
       image: novamindImg,
       name: 'Novamind',
-      link: 'https://github.com/Morganfp/novamind-ai-mobile-chatbot',
+      githubLink: 'https://github.com/Morganfp/novamind-ai-mobile-chatbot',
       about:
         'A mobile application that utilizes the OpenAI API to provide users with a conversational AI experience. The app features a user-friendly interface with an intuitive design that allows users to interact with the AI in a natural and seamless way.',
       tech: [
@@ -98,7 +100,7 @@ function Projects() {
     {
       image: frankieAIImg,
       name: 'Frankie AI',
-      link: 'https://github.com/Morganfp/frankie-ai',
+      githubLink: 'https://github.com/Morganfp/frankie-ai',
       about:
         'A locally hosted AI chatbot built to allow users to utilize multiple models, including Llama 3.1, Phi 3, Mistral, Gemma 2, and more. It runs entirely without an internet connection, featuring an intuitive interface and supporting diverse use cases.',
       tech: [
@@ -108,7 +110,7 @@ function Projects() {
     {
       image: cloudyCastImg,
       name: 'Cloudy Cast',
-      link: 'https://github.com/Morganfp/cloudy-cast-media-converter',
+      githubLink: 'https://github.com/Morganfp/cloudy-cast-media-converter',
       about:
         'A desktop application that utilizes the OpenCV and MoviePY Python libraries to convert between image and video formats such as jpg to png, mp4 to mov, and mkv to mp4.',
       tech: [
@@ -119,7 +121,7 @@ function Projects() {
     {
       image: mistyAIImg,
       name: 'Misty',
-      link: 'https://github.com/Morganfp/Misty-AI-Random-Fact',
+      githubLink: 'https://github.com/Morganfp/Misty-AI-Random-Fact',
       about:
         'A program that automates a web search using the Puppeteer Node.js library to scrape facts about the 50 US states and has the Misty robot speak a random fact using the Google text-to-speech API.',
       tech: [
@@ -147,12 +149,20 @@ function Projects() {
               />
               <div className="flex items-center gap-3 mt-4 mb-1 md:mt-5 md:mb-2">
                 <h2 className="section-subtitle">{proj.name}</h2>
-                {proj.link ? (
+                {proj.projectLink ? (
                   <button
-                    onClick={() => window.open(proj.link)}
+                    onClick={() => window.open(proj.projectLink)}
                     className="bg-[#00FFBB] py-1 px-2 rounded-2xl transform transition-all duration-300 hover:scale-110 cursor-pointer"
                   >
-                    <FaLink color="#212121" size={14} />
+                    <FaLink color="#212121" size={16} />
+                  </button>
+                ) : null}
+                {proj.githubLink ? (
+                  <button
+                    onClick={() => window.open(proj.githubLink)}
+                    className="bg-[#00FFBB] py-1 px-2 rounded-2xl transform transition-all duration-300 hover:scale-110 cursor-pointer"
+                  >
+                    <FaGithub color="#212121" size={16} />
                   </button>
                 ) : null}
               </div>
